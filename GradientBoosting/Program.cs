794fe3510b94d8aea88e49e62b3cc6f4075d21e5
KBR <kbr@ucl.dk>
2022-03-30 12:58:09 +0200

    Add project files.

---
+++ GradientBoosting/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+var sampleData = new GradientBoosting.GradientBoosting.ModelInput()
+{
+    Id = 2F,
+    SepalLengthCm = 49F,
+    SepalWidthCm = 30F,
+    PetalLengthCm = 14F,
+    PetalWidthCm = 2F,
+};
+
+//Load model and predict output
+var result = GradientBoosting.GradientBoosting.Predict(sampleData);
+
+Console.WriteLine($"{result.PredictedLabel}");
---
+++ RandomForest/Program.cs
@@ -0,0 +1,18 @@
+﻿//Load sample data
+
+using RandomForest;
+
+var sampleData = new RandomForestModel.ModelInput()
+{
+    Id = 2F,
+    SepalLengthCm = 49F,
+    SepalWidthCm = 30F,
+    PetalLengthCm = 14F,
+    PetalWidthCm = 2F,
+};
+
+//Load model and predict output
+FastForestWithOptions.Example();
+var result = RandomForestModel.Predict(sampleData);
+
+Console.WriteLine($"{result.PredictedLabel}");
